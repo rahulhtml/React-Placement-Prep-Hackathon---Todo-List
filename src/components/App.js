fef73d5489f7a37c46d43382276b45a4d77a4240
rahulhtml <82719075+rahulhtml@users.noreply.github.com>
2021-07-16 20:11:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React from "react";
-import "./../styles/App.css";
+import React,{useState} from 'react'
 
-function App() 
-{
+import Button1 from './Button1'
+import Button2 from './Button2'
+function App() {
+	const [count, setCount] = useState([])
+	function clickHandler(id){
+		setCount(count.filter((user,i)=>(i!==parseInt(id))))
+	}
+	// function editHandler(id){
+	// <Button2></Button2>
+	// }
 	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+		<div id="main">
+						<div>{
+			count.map((user,id)=>{
+			return < div key={id}>
+			<h1 className="list" >{user}</h1>
+			{/* <button onClick={()=> <Button2></Button2>}>edit</button> */}
+			<Button2 id={id}count={count} setCount={setCount}></Button2>
+			<button className="delete" onClick={()=>clickHandler(id)}>del</button>
+			</div>
+			})
+			
+		}
+		</div>
+		<Button1 fun={setCount}></Button1>
+		
+			</div>
+	)
 }
 
-
-export default App;
+export default App
